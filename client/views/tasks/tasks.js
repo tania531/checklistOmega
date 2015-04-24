df26ee38e4cf51fc7c5f02b39a30dd4a33db1eb4
Tania <tania.dev77@gmail.com>
2015-04-24 11:41:13 -0700

    first commit

---
+++ client/views/tasks/tasks.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('checklist')
+.controller('TasksCtrl', ['$scope', 'Task', '$window', function($scope, Task, $window){
+  Task.init();
+
+  $scope.addTask = function(task){
+    var o = {
+      title: task.title,
+      dueDate: task.dueDate.getTime(),
+      priority: task.priority,
+      isComplete: false,
+      createdAt: $window.Firebase.ServerValue.TIMESTAMP
+    };
+
+    Task.add(o)
+    .then(function(data){
+      console.info('data', data);
+    });
+  };
+}]);
